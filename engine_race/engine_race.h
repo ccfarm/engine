11170eece4e495da5b78cf7ea700e30b4c36f40a
wangchao.666 <wangchao.666@bytedance.com>
2018-11-10 10:46:07 +0800

    1
    Change-Id: I8653c1a1ce9b4264b7e95bd370c029846f6c5e35

--- engine_race/engine_race.h
+++ engine_race/engine_race.h
@@ -1,8 +1,12 @@
 // Copyright [2018] Alibaba Cloud All rights reserved
 #ifndef ENGINE_RACE_ENGINE_RACE_H_
 #define ENGINE_RACE_ENGINE_RACE_H_
+#include <pthread.h>
 #include <string>
 #include "include/engine.h"
+#include "util.h"
+#include "door_plate.h"
+#include "data_store.h"
 
 namespace polar_race {
 
@@ -10,7 +14,9 @@ class EngineRace : public Engine  {
  public:
   static RetCode Open(const std::string& name, Engine** eptr);
 
-  explicit EngineRace(const std::string& dir) {
+  explicit EngineRace(const std::string& dir) 
+    : mu_(PTHREAD_MUTEX_INITIALIZER),
+    db_lock_(NULL), plate_(dir), store_(dir) {
   }
 
   ~EngineRace();
@@ -30,6 +36,10 @@ class EngineRace : public Engine  {
       Visitor &visitor) override;
 
  private: 
+   pthread_mutex_t mu_;
+   FileLock* db_lock_;
+   DoorPlate plate_;
+   DataStore store_;
 
 };
 
