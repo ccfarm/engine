96fa05e7442d9ee050387b759a20d3512cf2d7ed
wangchao.666 <wangchao.666@bytedance.com>
2018-11-13 20:29:21 +0800

    16 * 4096
    Change-Id: Ie77ce505871bc2a83b573cc61088e8714e752220

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class EngineRace extends AbstractEngine {
 	final static long MAPSIZE = 12 * 1024 * 4 * 1024l;
 	//final static long FILESIZE =  4 * 1024 * 1024 * 1024;
-    final static long FILENUM =  4096 * 4;
+    final static long FILENUM =  4096 * 16;
 	String path;
 	//DiyHashMap position;
 	//LongLongHashMap position;
