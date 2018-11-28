a2e807f36be8976b6445b43a277dffa7da359baf
wangchao.666 <wangchao.666@bytedance.com>
2018-11-28 20:29:10 +0800

    1
    Change-Id: Ibe160076cb5b06c549663258106c73ba61dbf04d

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/qsortStore.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/qsortStore.java
@@ -21,7 +21,7 @@ public class qsortStore {
     public long[] keys;
     public int[] position;
     final private static int BUFFERSIZE = 150000;
-    final private static int PRE = 128;
+    final private static int PRE = 1024;
     //final private static int BUFFERSIZE = 500;
     long[] bkeys = new long[BUFFERSIZE];
     byte[][] bvalues = new byte[BUFFERSIZE][4096];
