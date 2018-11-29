5c9920a1e6bbdaa3ff7df1a3c2a7c5c4636d16f0
wangchao.666 <wangchao.666@bytedance.com>
2018-11-29 18:32:20 +0800

    16
    Change-Id: I9e350cdde1776e47547cfb86e2c1d64e0300a4b5

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -329,7 +329,7 @@ public class EngineRace extends AbstractEngine {
         }
         synchronized (this) {
             if (threadId == -1) {
-                qsortStore.pool = Executors.newFixedThreadPool(12);
+                qsortStore.pool = Executors.newFixedThreadPool(16);
                 threadId = Thread.currentThread().getId();
             }
         }
